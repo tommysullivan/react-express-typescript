13ffe81c9c1a02f670b3a07055706d52d216a20e
Tommy Sullivan <tsullivan@maprtech.com>
2018-04-29 21:32:49 -0700

    fixes #1

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# react-express-typescript
+react-express-typescript
+========================
+
+To get started, run `./ci` and see the options in the usage.
+
+    ./ci dev:environment:bash
+    
+To compile and run the web server, perform each of the following in its own terminal (or run in background):
+
+    ./ci dev:environment:tscWatch
+    ./ci dev:environment:express:run
+    
+To view the site, run:
+
+    ./ci dev:browser
---
+++ src/main/react-express-typescript.ts
@@ -0,0 +1,28 @@
+import * as express from "express";
+import * as http from "http";
+
+async function main() {
+    const expressApp = express();
+    const portNumber = 3000;
+    const hostName = 'localhost';
+    const myUrl = `http://${hostName}:${portNumber}`;
+
+    expressApp.set('port', portNumber);
+
+    ['src/main/public', 'node_modules'].forEach(
+        i => expressApp.use(express.static(i))
+    );
+
+    http.createServer(expressApp).listen(
+        portNumber,
+        (error:any) => {
+            if(error) {
+                console.log(error);
+                process.exit(1);
+            }
+            else console.log(`web server is running at ${myUrl}`);
+        }
+    );
+}
+
+main();
