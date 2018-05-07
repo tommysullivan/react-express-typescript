0fa5a89121f815e21dd89f63fa62dae816ac407c
Mike <Mike@Bobbys-MacBook-Pro.local>
2018-05-06 23:08:25 -0700

    played around with materials ui components and themes

--- src/main/browser/Site.tsx
+++ src/main/browser/Site.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
 import {Component, ReactElement} from "react";
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import {theme} from "./theme"
 
 export interface ISiteProps {
     title:string;
@@ -14,6 +21,26 @@ export class Site extends Component<ISiteProps, ISiteState> {
     }
 
     render():ReactElement<Site> {
-        return <p>Hello, {this.props.title}</p>
+        return <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
+            <AppBar
+                title="Title"
+                iconClassNameRight="muidocs-icon-navigation-expand-more"
+            />
+            <Card>
+                <CardHeader
+                    title="Without Avatar"
+                    subtitle="Subtitle"
+                    actAsExpander={true}
+                    showExpandableButton={true}
+                />
+                <CardActions>
+                <FlatButton label="Action1" />
+                <FlatButton label="Action2" />
+                </CardActions>
+                <CardText expandable={true}>
+                    hi tommy :)
+                </CardText>
+            </Card>
+        </MuiThemeProvider>
     }
 }
